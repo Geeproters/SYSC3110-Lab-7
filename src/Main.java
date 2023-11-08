357bc5acdbfd9602ddcc1ca089c7c301e0d4b9f0
peter <petergrose@cmail.carleton.ca>
2023-11-08 15:43:18 -0500

    Final Version. Added new lines to main to prove the subclasses behave differently.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+//Which IntelliJ-supported refactorings from the blog in step 1 were you able to use?
+// I was able to use the Deleting and renaming described in the blog. I didn't use the Extracting described.
+
 public class Main {
     public static void main(String[] args) {
-        LifelineSite site = new LifelineSite(100, 5.5);
+        ResidentialSite residentialSite = new ResidentialSite(100, 5.5);
+        System.out.println("Printing billable amount for a new Residential Site with units = 100 and rate = 5.5:");
+        System.out.println(residentialSite.getBillableAmount());
+        LifelineSite lifelineSite = new LifelineSite(100, 5.5);
         System.out.println("Printing billable amount for a new Lifeline Site with units = 100 and rate = 5.5:");
-        System.out.println(site.getBillableAmount());
+        System.out.println(lifelineSite.getBillableAmount());
     }
 }
