ab1587ba72993465a2fca2149e7bc8706abbf7e1
peter <petergrose@cmail.carleton.ca>
2023-11-08 15:21:23 -0500

    Safe-deleted getBillableAmount() in subclasses using IntelliJ's safe delete feature. This told me that I had usages in main(), so I changed that usage to use the new abstract methods as well.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         LifelineSite site = new LifelineSite(100, 5.5);
         System.out.println("Printing billable amount for a new Lifeline Site with units = 100 and rate = 5.5:");
-        System.out.println(site.getBillableAmount());
+        System.out.println(site.getBaseAmount() + site.getTaxAmount());
     }
 }
